85ec2c5c34eafe9df8315dc33c9ea1feb49bc1b1
Andrew Bowen <andrew.james.bowen@gmail.com>
2018-05-13 16:05:56 +1000

    Added basic add question functionality

--- InterviewRubric.Shared/AppState.cs
+++ InterviewRubric.Shared/AppState.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Linq;
 
 namespace InterviewRubric.Shared
@@ -12,28 +11,33 @@ namespace InterviewRubric.Shared
 
         public void SetInitialState(RubricQuestion[] questions)
         {
-            RubricQuestions = questions;
+            RubricQuestions = questions.ToList();
             UnselectedRubricQuestions = questions.ToList();
             SelectedRubricQuestions = new List<RubricQuestion>();
         }
 
-        public IEnumerable<RubricQuestion> RubricQuestions;
+        public List<RubricQuestion> RubricQuestions;
         public List<RubricQuestion> SelectedRubricQuestions;
         public List<RubricQuestion> UnselectedRubricQuestions;
 
         public void AddQuestion(RubricQuestion question)
         {
-            Console.WriteLine($"Add : {question}");
+            RubricQuestions.Add(question);
+            UnselectedRubricQuestions.Add(question);
+            NotifyStateChanged();   
+        }
+
+        public void SelectQuestion(RubricQuestion question)
+        {
             if (UnselectedRubricQuestions.Contains(question))
             {
                 UnselectedRubricQuestions.Remove(question);
                 SelectedRubricQuestions.Add(question);
                 NotifyStateChanged();
-                Console.WriteLine($"Added : {question}");
             }
         }
 
-        public void RemoveQuestion(RubricQuestion question)
+        public void UnselectQuestion(RubricQuestion question)
         {
             if (SelectedRubricQuestions.Contains(question))
             {
--- InterviewRubric.Shared/RubricQuestion.cs
+++ InterviewRubric.Shared/RubricQuestion.cs
@@ -2,17 +2,17 @@
 {
     public class RubricQuestion
     {
+        // TODO: Move strings to Markdown
         public string Question { get; set; }
         public string Low { get; set; }
         public string Medium { get; set; }
         public string High { get; set; }
         public Difficulty Difficulty { get;set; }
         public int Minutes { get; set; }
-        public bool Selected { get; set; }
 
         public override string ToString()
         {
-            return $"{Selected} : {Question}";
+            return $"{Question} : {Minutes}";
         }
     }
 }
