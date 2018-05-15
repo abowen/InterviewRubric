3cf039d476cc3ddfecdb59aefa3ab6821da244d2
Andrew Bowen <andrew.bowen@gmail.com>
2018-05-16 08:01:33 +1000

    Cleaned up getting a list of enums

---
+++ InterviewRubric.Shared/Enums/Utils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InterviewRubric.Shared.Enums
+{
+    public static class Enum<T>
+    {
+        public static IEnumerable<T> GetEnums()
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>();
+        }
+    }
+}
